5d3b5987f241ea7003e642b015c7b23fd77e6fdc
divyanx <divyansh.2488@gmail.com>
2020-11-28 13:15:33 +0530

    rotating virus, textbox

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ clock = pygame.time.Clock()
 FPS = 60
 game_folder = path.dirname(__file__)
 img_folder = path.join(game_folder,"img")
+score = 0
+font_name = pygame.font.match_font("comicsansms")
 #Game Classes
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -25,6 +27,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img,(60,60))
         #self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
+        self.radious = int(self.rect.width*.9/2)
+        #pygame.draw.circle(self.image,GREEN,self.rect.center,self.radious)
         self.rect.centerx = WIDTH/2
         self.rect.bottom = HEIGHT - 10  #sepration between bottom and ship
         self.speed_x = 0
@@ -61,12 +65,28 @@ class Player(pygame.sprite.Sprite):
 class Corona(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = random.choice((corona_img))
+        self.original_image = random.choice((corona_img))
+        self.image = self.original_image.copy()
         self.rect = self.image.get_rect()
+        self.radious = int(self.rect.width * .9 / 2)
         self.rect.x = random.randrange(0,WIDTH-self.rect.width)
         self.rect.y = random.randrange(-150,-100)
         self.speed_y = random.randrange(2,8)
         self.speed_x = random.randrange(-3,3)  #x direction motion
+        self.last_rotation = pygame.time.get_ticks()
+        self.rotation_degree = 0
+        self.rotation_speed = 5
+    def rotate(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_rotation  >200:
+            self.last_rotation = current_time
+            self.rotation_degree += self.rotation_speed
+            old_center = self.rect.center
+            self.image = pygame.transform.rotate(self.original_image,self.rotation_degree)
+            self.rect = self.image.get_rect()
+
+            #pygame.draw.circle(self.image, GREEN, self.rect.center, self.radious)
+            self.rect.center = old_center
     def spawn_new_corona(self):
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(-150, -100)
@@ -79,6 +99,7 @@ class Corona(pygame.sprite.Sprite):
         self.rect.y += self.speed_y
         self.rect.x += self.speed_x
         self.boundary()
+        self.rotate()
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -104,15 +125,25 @@ def get_image(filename, colorkey = None):
     img = pygame.image.load(path.join(img_folder,filename)).convert()
     img.set_colorkey(colorkey)
     return img
+def message_to_screen(message,color,font_size,x,y):
+    font = pygame.font.SysFont(font_name,font_size)
+    text = font.render(message,True,color)
+    text_rect = text.get_rect()
+    text_rect.center = (x,y)
+    screen.blit(text,text_rect)
 
 #images
-background = get_image("background.png")
+background = get_image("background.png",BLACK)
 background_rect = background.get_rect()
-player_img = get_image("player2.png",WHITE)  #7
+player_img = get_image("player1.png",BLACK)  #7
 bullet_img = get_image("bullet1.png",BLACK)
 corona_img = []
+# for i in range(1,6):
+#     img = get_image("virus{}.png".format(i))
+#     corona_img.append(img)
 for i in range(1,6):
-    img = get_image("virus{}.png".format(i))
+    img = get_image("player1.png",WHITE)
+    corona_img.append(img)
 #player_img =
 #player_img = get_image("playerShip.png")
 #Game sprites
@@ -138,7 +169,7 @@ while running:
     all_sprites.update()
 
     #checking ship collisions
-    corona_collision = pygame.sprite.spritecollide(player,all_corona,False)
+    corona_collision = pygame.sprite.spritecollide(player,all_corona,False,pygame.sprite.collide_circle)
     if corona_collision:
         running = False
 
